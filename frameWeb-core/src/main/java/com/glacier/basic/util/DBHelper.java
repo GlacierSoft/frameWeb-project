e17f6c2d0f437245482696f0e12b641b23dfcbad
dongxichao <406592176@qq.com>
2014-08-18 11:16:18 +0800

    提交类优化。

--- frameWeb-core/src/main/java/com/glacier/basic/util/DBHelper.java
+++ frameWeb-core/src/main/java/com/glacier/basic/util/DBHelper.java
@@ -21,9 +21,9 @@ import java.util.Properties;
 import org.springframework.core.io.ClassPathResource;
 
 /**
- * @ClassName: DBHelper 
- * @Description: TODO(获取数据库连接) 
- * @author xichao.dong	
+ * @ClassName: DBHelper
+ * @Description: TODO(获取数据库连接)
+ * @author xichao.dong
  * @email 406592176@qq.com
  * @date 2014-8-15 上午11:12:05
  */
@@ -32,7 +32,7 @@ public final class DBHelper {
     public static Connection getConnection() {
         Connection conn = null;
         try {
-            //通过读取配置文件获取jdbc连接参数
+            // 通过读取配置文件获取jdbc连接参数
             ClassPathResource classPathResource = new ClassPathResource("spring/config.properties");
             Properties p = new Properties();
             p.load(classPathResource.getInputStream());
@@ -56,6 +56,7 @@ public final class DBHelper {
 
     /**
      * 增删改【Add、Del、Update】
+     * 
      * @param sql
      * @return int
      */
@@ -77,424 +78,229 @@ public final class DBHelper {
     }
 
     /**
-     * 
      * 增删改【Add、Delete、Update】
      * 
-     * 
-     * 
      * @param sql
-     * 
      * @param obj
-     * 
      * @return int
      */
-
     public static int executeNonQuery(String sql, Object... obj) {
-
         int result = 0;
-
         Connection conn = null;
-
         PreparedStatement pstmt = null;
-
         try {
-
             conn = getConnection();
-
             pstmt = conn.prepareStatement(sql);
-
             for (int i = 0; i < obj.length; i++) {
-
                 pstmt.setObject(i + 1, obj[i]);
-
             }
-
             result = pstmt.executeUpdate();
-
         } catch (SQLException err) {
-
             err.printStackTrace();
-
             free(null, pstmt, conn);
-
         } finally {
-
             free(null, pstmt, conn);
-
         }
-
         return result;
-
     }
 
     /**
-     * 
      * 查【Query】
      * 
-     * 
-     * 
      * @param sql
-     * 
      * @return ResultSet
      */
-
     public static ResultSet executeQuery(String sql) {
-
         Connection conn = null;
-
         Statement stmt = null;
-
         ResultSet rs = null;
-
         try {
-
             conn = getConnection();
-
             stmt = conn.createStatement();
-
             rs = stmt.executeQuery(sql);
-
         } catch (SQLException err) {
-
             err.printStackTrace();
-
             free(rs, stmt, conn);
-
         }
-
         return rs;
-
     }
 
     /**
-     * 
      * 查【Query】
      * 
-     * 
-     * 
      * @param sql
-     * 
      * @param obj
-     * 
      * @return ResultSet
      */
-
     public static ResultSet executeQuery(String sql, Object... obj) {
-
         Connection conn = null;
-
         PreparedStatement pstmt = null;
-
         ResultSet rs = null;
-
         try {
-
             conn = getConnection();
-
             pstmt = conn.prepareStatement(sql);
-
             for (int i = 0; i < obj.length; i++) {
-
                 pstmt.setObject(i + 1, obj[i]);
-
             }
-
             rs = pstmt.executeQuery();
-
         } catch (SQLException err) {
-
             err.printStackTrace();
-
             free(rs, pstmt, conn);
-
         }
-
         return rs;
-
     }
 
     /**
-     * 
      * 判断记录是否存在
      * 
-     * 
-     * 
      * @param sql
-     * 
      * @return Boolean
      */
-
     public static Boolean isExist(String sql) {
-
         ResultSet rs = null;
-
         try {
-
             rs = executeQuery(sql);
-
             rs.last();
-
             int count = rs.getRow();
-
             if (count > 0) {
-
                 return true;
-
             } else {
-
                 return false;
-
             }
-
         } catch (SQLException err) {
-
             err.printStackTrace();
-
             free(rs);
-
             return false;
-
         } finally {
-
             free(rs);
-
         }
-
     }
 
     /**
-     * 
      * 判断记录是否存在
      * 
-     * 
-     * 
      * @param sql
-     * 
      * @return Boolean
      */
-
     public static Boolean isExist(String sql, Object... obj) {
-
         ResultSet rs = null;
-
         try {
-
             rs = executeQuery(sql, obj);
-
             rs.last();
-
             int count = rs.getRow();
-
             if (count > 0) {
-
                 return true;
-
             } else {
-
                 return false;
-
             }
-
         } catch (SQLException err) {
-
             err.printStackTrace();
-
             free(rs);
-
             return false;
-
         } finally {
-
             free(rs);
-
         }
-
     }
 
     /**
-     * 
      * 获取查询记录的总行数
      * 
-     * 
-     * 
      * @param sql
-     * 
      * @return int
      */
-
     public static int getCount(String sql) {
-
         int result = 0;
-
         ResultSet rs = null;
-
         try {
-
             rs = executeQuery(sql);
-
             rs.last();
-
             result = rs.getRow();
-
         } catch (SQLException err) {
-
             free(rs);
-
             err.printStackTrace();
-
         } finally {
-
             free(rs);
-
         }
-
         return result;
-
     }
 
     /**
-     * 
      * 获取查询记录的总行数
      * 
-     * 
-     * 
      * @param sql
-     * 
      * @param obj
-     * 
      * @return int
      */
-
     public static int getCount(String sql, Object... obj) {
-
         int result = 0;
-
         ResultSet rs = null;
-
         try {
-
             rs = executeQuery(sql, obj);
-
             rs.last();
-
             result = rs.getRow();
-
         } catch (SQLException err) {
-
             err.printStackTrace();
-
         } finally {
-
             free(rs);
-
         }
-
         return result;
-
     }
 
     /**
-     * 
      * 释放【ResultSet】资源
      * 
-     * 
-     * 
      * @param rs
      */
-
     public static void free(ResultSet rs) {
-
         try {
-
             if (rs != null) {
-
                 rs.close();
-
             }
-
         } catch (SQLException err) {
-
             err.printStackTrace();
-
         }
-
     }
 
     /**
-     * 
      * 释放【Statement】资源
      * 
-     * 
-     * 
      * @param st
      */
-
     public static void free(Statement st) {
-
         try {
-
             if (st != null) {
-
                 st.close();
-
             }
-
         } catch (SQLException err) {
-
             err.printStackTrace();
-
         }
-
     }
 
     /**
-     * 
      * 释放【Connection】资源
      * 
-     * 
-     * 
      * @param conn
      */
-
     public static void free(Connection conn) {
-
         try {
-
             if (conn != null) {
-
                 conn.close();
-
             }
-
         } catch (SQLException err) {
-
             err.printStackTrace();
-
         }
-
     }
 
     /**
-     * 
      * 释放所有数据资源
      * 
-     * 
-     * 
      * @param rs
-     * 
      * @param st
-     * 
      * @param conn
      */
-
     public static void free(ResultSet rs, Statement st, Connection conn) {
-
         free(rs);
-
         free(st);
-
         free(conn);
-
     }
-
 }
